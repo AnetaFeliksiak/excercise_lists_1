e3a3dfd4eefdd6fddc020d15edba8d5823da8258
Aneta Feliksiak <feliksiakaneta@gmail.com>
2019-11-22 12:01:11 +0000

    Add test to read file

---
+++ file_manager.py
@@ -0,0 +1,12 @@
+def read_from_file(filename="default_words.txt"):
+    '''
+    Reads lists from file
+
+    >>> read_from_file()
+        [["aaa", "bbbb", "cccccc"], ["dd", "eeeeee", "fffffffff"]]
+    '''
+    pass
+
+
+def write_to_file(words, filename="longest_words.txt"):
+    pass
