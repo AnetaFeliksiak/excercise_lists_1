b8b67b7c54250fbdab4b202d10f8e5ab9c1b3f32
Aneta Feliksiak <feliksiakaneta@gmail.com>
2019-11-22 12:28:25 +0000

    Implement read file function

--- file_manager.py
+++ file_manager.py
@@ -1,12 +1,19 @@
-def read_from_file(filename="default_words.txt"):
+def read_from_file(filename='default_words.txt'):
     '''
     Reads lists from file
 
     >>> read_from_file()
-        [["aaa", "bbbb", "cccccc"], ["dd", "eeeeee", "fffffffff"]]
+    [['aaa', 'bbbb', 'cccccc'], ['dd', 'eeeeee', 'fffffffff']]
     '''
-    pass
+    list_of_lines = []
+    file = open(filename, 'r')
+    for line in file:
+        line = line.strip().split(',')
+        stripped_elements = [element.strip() for element in line]
+        list_of_lines.append(stripped_elements)
+    file.close()
+    return list_of_lines
 
 
-def write_to_file(words, filename="longest_words.txt"):
+def write_to_file(words, filename='longest_words.txt'):
     pass
