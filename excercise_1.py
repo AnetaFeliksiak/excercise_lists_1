a26c1847cafe2436d4c2379d8504de900505a7ce
Aneta Feliksiak <feliksiakaneta@gmail.com>
2019-11-23 11:26:38 +0000

    Implement find the longest word function

---
+++ excercise_1.py
@@ -0,0 +1,30 @@
+import file_manager
+
+
+def find_the_longest_word(list_of_lines):
+    '''
+    Find the longest word and its paramethers
+    
+    >>> find_the_longest_word(file_manager.read_from_file("words.txt"))
+    [['ciasteczko', '10', '2'], ['krzysztofu', '10', '4']]
+    '''
+
+    max_length = 0
+    max_item = ""
+    max_index = 0
+    longest_words = []
+
+    for index in range(len(list_of_lines)):
+        for item in list_of_lines[index]:
+            if len(item) > max_length:
+                longest_words.clear()
+                max_length = len(item)
+                max_item = item
+                max_index = index
+                longest_words.append([max_item, str(max_length), str(max_index)])
+            elif len(item) == max_length:
+                max_length = len(item)
+                max_item = item
+                max_index = index
+                longest_words.append([max_item, str(max_length), str(max_index)])
+    return longest_words
